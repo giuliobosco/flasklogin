c7464068fa5bf2c5db73289d5161b21e0dcb97f6
giuliobosco <giuliobva@gmail.com>
2021-03-05 20:04:05 +0100

    add config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+""" Flask app configuration """
+
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = path.abspath(path.dirname(__file__))
+
+load_dotenv(path.join(basedir, '.env'))
+
+
+class Config:
+    """Set Flask configuration environments variables"""
+
+    FLASK_APP = 'wsgi.py'
+    FLASK_ENV = environ.get('FLASK_ENV')
+    FLASK_KEY = environ.get('SECRET_KEY')
+
+    # flask assets
+    LESS_BIN = environ.get('LESS_BIN')
+    ASSETS_DEBUG = environ.get('ASSETS_DEBUG')
+    LESS_RUN_IN_DEBUG = environ.get('LESS_RUN_IN_DEBUG')
+
+    # static assets
+    STATIC_FOLDER = 'static'
+    TEMPLATES_FOLDER = 'templates'
+    COMPRESSOR_DEBUG = environ.get('COMPRESSOR_DEBUG')
+
+    # Flask-SQLAlchemy
+    SQLALCHEMY_DATABASE_URI = environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
