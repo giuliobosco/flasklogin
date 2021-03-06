2bdd7cbe00022d7a7a8c799f8382047ba74db630
giuliobosco <giuliobva@gmail.com>
2021-03-06 09:52:37 +0100

    add flask_login_tutorial/__init__.py

---
+++ flask_login_tutorial/__init__.py
@@ -0,0 +1,37 @@
+"""Initialize app."""
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+login_manager = LoginManager()
+
+def create_app():
+    """Construct the core app object."""
+    app = Flask(__name__, instance_relative_config=False)
+    
+    # Application Configuration
+    app.config.from_object('config.Config')
+
+    # Initialize plugins
+    db.init_app(app)
+    login_manager.init_app(app)
+
+    with app.app_context():
+        from . import routes
+        from . import auth
+        from .assets import compile_assets
+
+        # Register Blueprints
+        app.register_blueprint(routes.main_bp)
+        app.register_blueprint(auth.main_bp)
+
+        # create database models
+        db.create_all()
+
+        # compile static assets
+        if app.config['FLASK_ENV'] == 'development':
+            compile_assets(app)
+
+    return app
