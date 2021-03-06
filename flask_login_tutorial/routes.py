75825e26a4ad7e0ed4c8ba3877049127c4e1ad05
giuliobosco <giuliobva@gmail.com>
2021-03-06 12:45:39 +0100

    add layouts & routes

---
+++ flask_login_tutorial/routes.py
@@ -0,0 +1,34 @@
+"""Logged-in page routes."""
+from flask import Blueprint, render_template, redirect, url_for
+from flask_login import current_user, login_required, logout_user
+
+
+# Blueprint Configuration
+main_bp = Blueprint(
+    'main_bp',
+    __name__,
+    template_folder='templates',
+    static_folder='static'
+)
+
+@main_bp.route('/', methods=['GET'])
+@login_required
+def dashboard():
+    """Logged-in User Dashboard"""
+    return render_template(
+        'dashboard.jinja2',
+        title='Flask-login tutorial',
+        template='dashboard-template',
+        current_user=current_user,
+        body="You are now logged-in"
+    )
+
+
+@main_bp.route('/logout')
+@login_required
+def logout():
+    """User log-out login."""
+    logout_user()
+    return redirect(url_for('auth_bp.login'))
+
+
