04da28f5c21b410d189668dd6e0ee826150af443
giuliobosco <giuliobva@gmail.com>
2021-03-06 22:26:26 +0100

    add flt/init - csrf

--- flask_login_tutorial/__init__.py
+++ flask_login_tutorial/__init__.py
@@ -3,20 +3,24 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_wtf.csrf import CsrfProtect
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+csrf = CsrfProtect()
 
 def create_app():
     """Construct the core app object."""
     app = Flask(__name__, instance_relative_config=False)
-    
+
     # Application Configuration
     app.config.from_object('config.Config')
+    app.secret_key='aa'
 
     # Initialize plugins
     db.init_app(app)
     login_manager.init_app(app)
+    csrf.init_app(app)
 
     with app.app_context():
         from . import routes
